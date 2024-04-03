adea1c79a9c35c7a3326a14652a6b41702614b03
WRD <dandobilly@gmail.com>
2024-04-02 21:57:31 -0400

    wrote routes for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,29 @@
-const apiRoutes = require('express').Router();
-const { readAndAppend, ReadFromFile } = require()
+const apr = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+apr.get('/notes', (req, res) => 
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+apr.post('/notes', (req, res) => {
+    const note = req.body;
+    
+    if (note) {
+        const newNote = {
+            note,
+            note_id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        res.json(response);
+    } else {
+        res.json('Error in posting Note');
+    }
+});
+
+module.exports = apr;
