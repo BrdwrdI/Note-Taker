baf6a2a1f8b5fb29ca08f98745c21dbdcb21c166
WRD <dandobilly@gmail.com>
2024-04-07 20:08:57 -0400

    changed port entry to 1337

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes.js');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 1337;
 
 const app = express();
 
